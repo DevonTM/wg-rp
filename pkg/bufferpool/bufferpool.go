ca62e5ebb5f3b02fe1380461753447805151dde1
DevonTM <ipangcheater@gmail.com>
2025-07-30 19:33:27 +0700

    Refactor BufferPool to improve buffer management and ensure proper usage of sync.Pool

--- pkg/bufferpool/bufferpool.go
+++ pkg/bufferpool/bufferpool.go
@@ -8,6 +8,7 @@ import (
 // BufferPool manages a pool of byte buffers for efficient I/O operations
 type BufferPool struct {
 	pool sync.Pool
+	size int
 }
 
 // NewBufferPool creates a new buffer pool with the specified buffer size
@@ -15,21 +16,23 @@ func NewBufferPool(bufferSize int) *BufferPool {
 	return &BufferPool{
 		pool: sync.Pool{
 			New: func() any {
-				buf := make([]byte, bufferSize)
-				return &buf
+				return make([]byte, bufferSize)
 			},
 		},
+		size: bufferSize,
 	}
 }
 
 // Get retrieves a buffer from the pool
 func (bp *BufferPool) Get() []byte {
-	return *bp.pool.Get().(*[]byte)
+	return bp.pool.Get().([]byte)
 }
 
 // Put returns a buffer to the pool
 func (bp *BufferPool) Put(buf []byte) {
-	bp.pool.Put(&buf)
+	if len(buf) == bp.size {
+		bp.pool.Put(buf)
+	}
 }
 
 // CopyWithBuffer copies from src to dst using a buffer from the pool
